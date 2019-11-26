cd3b8006c8e4088a9c3958bd53214921a00371ed
Md Tanvir Hasan Anik <tanvir@localhost.localdomain>
2019-11-26 22:16:34 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+Route::view('/', 'welcome');
+Auth::routes();
+
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+Route::get('/login/faculty', 'Auth\LoginController@showFacultyLoginForm');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/register/faculty', 'Auth\RegisterController@showFacultyRegisterForm');
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/blogger', 'Auth\LoginController@bloggerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+Route::post('/register/faculty', 'Auth\RegisterController@createFaculty');
+
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin')->middleware('auth:admin');
+Route::view('/faculty', 'faculty')->middleware('auth:faculty');
